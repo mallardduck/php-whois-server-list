35e25ac2900e192abbf990b90e123ab95bd92599
Dan Pock <self@danpock.me>
2021-07-29 12:54:47 -0400

    fix: fix broken tests on windows

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -22,7 +22,10 @@ it('will throw an exception with URL provided as path', function () {
     $this->expectException(JsonException::class);
     $this->expectExceptionCode(0);
     $this->expectExceptionMessageMatches(
-        '#Cannot get source file from path: ([\/a-zA-Z\-]+)/tests/MockLocators/../../tmp/empty.json#',
+        '#Cannot get source file from path: ([\/a-zA-Z\-\:\\]+)' .
+        DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'MockLocators' .
+        DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .
+        'tmp' . DIRECTORY_SEPARATOR . 'empty.json#',
     );
 
     new InvalidFileTestLocator();
